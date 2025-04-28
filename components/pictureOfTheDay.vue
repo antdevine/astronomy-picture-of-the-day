369c9365dddbda0fb8267bbd36e2feebc8278465
antdevine <antdevine@hotmail.co.uk>
2025-04-28 21:04:16 +0100

    Added if statement to the image, title and description so they dont render unless they exist

--- components/pictureOfTheDay.vue
+++ components/pictureOfTheDay.vue
@@ -8,6 +8,7 @@ const { data: apod, pending, error } = await useFetch('/api/apod');
     <div v-else-if="error">Failed to load.</div>
     <div v-else>
         <NuxtImg
+        v-if="apod.hdImage || apod.image"
         :src="apod.hdImage ? apod.hdImage : apod.image"
         :sizes="'(max-width: 767px) 100vw, 1600px'"
         format="webp"
@@ -15,8 +16,8 @@ const { data: apod, pending, error } = await useFetch('/api/apod');
         loading="lazy"
         :alt="apod.title"
         />
-      <h1>{{ apod.title }}</h1>
-      <p>{{ apod.description }}</p>
+      <h1 v-if="apod.title">{{ apod.title }}</h1>
+      <p v-if="apod.description">{{ apod.description }}</p>
     </div>
   </div>
 </template>
