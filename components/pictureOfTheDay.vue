c8f183ae5c7abb40a5b81c06bcf55b987d0a36f0
antdevine <antdevine@hotmail.co.uk>
2025-04-28 21:44:27 +0100

    Installed tailwind, added tailwind styles to make page look like a card format

--- components/pictureOfTheDay.vue
+++ components/pictureOfTheDay.vue
@@ -6,7 +6,7 @@ const { data: apod, pending, error } = await useFetch('/api/apod');
     <div>
     <div v-if="pending">Loading...</div>
     <div v-else-if="error">Failed to load.</div>
-    <div v-else>
+    <div v-else class="container mx-auto p-4 max-w-2xl bg-white">
         <NuxtImg
         v-if="apod.hdImage || apod.image"
         :src="apod.hdImage ? apod.hdImage : apod.image"
@@ -15,8 +15,9 @@ const { data: apod, pending, error } = await useFetch('/api/apod');
         quality="80"
         loading="lazy"
         :alt="apod.title"
+        class="rounded-lg shadow-lg mb-4 max-h-[80vh] object-cover"
         />
-      <h1 v-if="apod.title">{{ apod.title }}</h1>
+      <h1 class="text-3xl font-bold underline mb-2" v-if="apod.title">{{ apod.title }}</h1>
       <p v-if="apod.description">{{ apod.description }}</p>
     </div>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/image'],
   runtimeConfig: {
     NASA_API: process.env.NASA_API,
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
